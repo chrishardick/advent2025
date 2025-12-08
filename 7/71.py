fc4f8a5e11d8bae4c3c59bfef28726d5674f55c3
Chris Hardick <chardick@icloud.com>
2025-12-08 07:05:18 -0500

    71 - start

---
+++ 7/71.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+#==========
+# 71
+#==========
+
+import sys
+
+#
+# main
+#
+
+matrix = []
+
+start = None
+
+x=0
+y=0
+
+for line in sys.stdin:
+
+    line = line.rstrip()    # remove any white space from end of string
+
+    ll = []
+
+    for c in line
+        ll.append(c)
+        if c == "S":
+            start = (x,y)
+
+        x += 1
+
+    matrix.append (ll)
+
+    y += 1
+
+if start == None:
+    print ("start not found")
+    sys.exit(1)
+
+x = start[0]
+
+
+for y in range (start[1]+1,len(matrix))
+
+    
+    
+    
+
