bdfa40b2d1d5183871071d823795ebd21bff2ea1
chardick <chardick1@bloomberg.net>
2025-12-08 14:01:21 -0500

    71

--- 7/71.py
+++ 7/71.py
@@ -22,7 +22,7 @@ for line in sys.stdin:
 
     ll = []
 
-    for c in line
+    for c in line:
         ll.append(c)
         if c == "S":
             start = (x,y)
@@ -37,11 +37,28 @@ if start == None:
     print ("start not found")
     sys.exit(1)
 
-x = start[0]
+split_count = 0
 
+print ("line len=",len(matrix[0]))
+print ("#lines=",len(matrix))
 
-for y in range (start[1]+1,len(matrix))
+for y in range (1,len(matrix)):
 
+    for x in range (0,len(matrix[0])):
+
+        if matrix[y-1][x] == 'S' or matrix[y-1][x] == '|':
+            if matrix[y][x] == '.':
+                matrix[y][x] = '|'
+            elif matrix[y][x] == '^':
+                split_count += 1
+                print ("split at", x, y)
+
+                if x-1 >= 0:
+                    matrix[y][x-1] = '|'
+                if x+1 <= len(matrix[0])-1:
+                    matrix[y][x+1] = '|'
+
+print ("split_count=",split_count)
     
     
     
